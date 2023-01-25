2df79b09c463f4acc062119482b1f19190f54638
Rounilamas <121729428+Rounilamas@users.noreply.github.com>
2023-01-24 23:10:53 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+$('#btn_primary').click(function(){
+    Swal.fire({
+        position: 'top-end',
+        icon: 'error',
+        title: 'Esta novela no esta disponible para su descarga',
+        showConfirmButton: false,
+        timer: 1500,
+        width:'22em'
+      })
+});
+
+$('#btn_primary2').click(function(){
+    Swal.fire({
+        position: 'top-end',
+        icon: 'error',
+        title: 'Esta novela no esta disponible para su descarga',
+        showConfirmButton: false,
+        timer: 1500,
+        width:'22em'
+      })
+});

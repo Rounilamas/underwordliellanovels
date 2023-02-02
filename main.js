8a76a2359acea380cb0754a5bba4431e7410c34d
Rounilamas <121729428+Rounilamas@users.noreply.github.com>
2023-02-02 13:09:07 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -19,3 +19,14 @@ $('#btn_primary2').click(function(){
         width:'22em'
       })
 });
+
+$('#btn_primary3').click(function(){
+  Swal.fire({
+      position: 'top-end',
+      icon: 'error',
+      title: 'La descarga solo esta disponible por Mega',
+      showConfirmButton: false,
+      timer: 1500,
+      width:'22em'
+    })
+});
